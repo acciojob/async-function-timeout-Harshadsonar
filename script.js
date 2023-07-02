c422782956d7d2b0c37b1e7fd18b6cfbae9c2b6f
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-02 18:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
+
+async function displayMessage(){
+	const tect = textInput.value;
+	const delay = delayInput.value;
+
+	outputDiv.textContent = '';
+
+	await new Promise(resolve = setTimeout(resolve, elay));
+
+	outputDiv.tectContent = text;
+}
+btn.addEventListener('click', displayMessage);
