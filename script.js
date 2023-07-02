764bc29e77f4f3f611d8c9e9347c5b1cf644c3a9
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-02 18:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const delayInput = document.getElementById('delay');
 const btn = document.getElementById('btn');
 const outputDiv = document.getElementById('output');
 
-async function displayMessage(){
-	const tect = textInput.value;
-	const delay = delayInput.value;
+async function displayMessage() {
+  const text = textInput.value;
+  const delay = delayInput.value;
 
-	outputDiv.textContent = '';
+  outputDiv.textContent = '';
 
-	await new Promise(resolve = setTimeout(resolve, elay));
-
-	outputDiv.tectContent = text;
+  try {
+    await new Promise((resolve) => setTimeout(resolve, delay));
+    outputDiv.textContent = text;
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+
 btn.addEventListener('click', displayMessage);
